89332afcb62873ce9a52f2dd1e838e7e51f5610f
Leonardo <20230070@ricaldone.edu.sv>
2024-03-01 15:56:56 -0600

    Menu estructura

--- Pagina/Sitio Web Abstrait/js/cargarMenuIndex.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function(){
-    const contenedorMenu = document.getElementById("menuContenedor");
-    const menuHTML = generarMenuIndex();
-
-    contenedorMenu.innerHTML = menuHTML;
-});
--- Pagina/Sitio Web Abstrait/js/cargarMenuPages.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function(){
-    const contenedorMenu = document.getElementById("menuContenedor");
-    const menuHTML = generarMenuPages();
-
-    contenedorMenu.innerHTML = menuHTML;
-});
