adf0aac5c57bc30c5747eea4bc89c361bba768ed
Emily <20230089@ricaldone.edu.sv>
2024-03-03 21:07:44 -0600

    Img del carousel y cambios en el footer

--- Aerolínea/js/footerIndex.js
+++ Aerolínea/js/footerIndex.js
@@ -1,44 +1,21 @@
 function generarFooterIndex()
 {
-    const footerIndex = `  <footer class=" text-white py-3 mt-5">
-    <div class="container-lg mt-3">
-      <div class="row">
-        <div class="col">
-          <p class="texto1">Abstrait</p>
-          <p class="texto2">abstrait.app@gmail.com</p>
-          <br>
-          <a href="pages/PreguntasFrecuentes.html">Preguntas Frecuentes</a>
-          <br>
-          <a href="pages/sobreNosotros.html">Sobre Nosotros</a>
+    const footerIndex = `<footer class="bg-dark text-white text-center py-3 mt-5">
+    <div class="container text-center">
+        <div class="row row-cols-12">
+            <div class="col">
+                <section>
+                    <div id="flexfooter">
+                    </div>
+                    </section>
+                    <br>
+                </div>
+            </div>
         </div>
-        <div class="col-2 col-lg-2 col-md-2">
-          <div class="linea"></div>
-        </div>
-        <div class="col">
-          <p class="texto1">&#160 Síguenos</p>
-          <br>
-          <a href="https://www.instagram.com/abstrait.app/" target="_blank">&#160 Instagram</a>
-          <br>
-          <a href="https://twitter.com/Abstraitapp" target="_blank">&#160 Twitter</a>
-          <br>
-          <a href=https://www.tiktok.com/@abstrait.app?is_from_webapp=1&sender_device=pc" target="_blank">&#160 TikTok</a>
-          <br>
-          <br>
-          <a class="btn bg-transparent shadow-none" href="https://www.instagram.com/abstrait.app/" target="_blank" role="button">
-            <img src="img/instagram.png" width="30" />
-          </a>
-          <a class="btn bg-transparent shadow-none" href="https://twitter.com/Abstraitapp" target="_blank" role="button">
-            <img src="img/xLogo.png" width="30" />
-          </a>
-          <a class="btn bg-transparent shadow-none" href=https://www.tiktok.com/@abstrait.app?is_from_webapp=1&sender_device=pc" target="_blank" role="button">
-            <img src="img/tiktok.png" width="30" />
-          </a>
-        </div>
-      </div>
-    </div>
-    <br>
-    <p class="textoA">&#169 2023 Abstrait. Todos los Derechos Reservados.</p>
-  </footer>`;
+        <center>
+            <p><b>Copyright &copy; Equipo de Aerolínea</b></p>
+        </center>
+       </footer>`;
 
   return footerIndex;
 }
